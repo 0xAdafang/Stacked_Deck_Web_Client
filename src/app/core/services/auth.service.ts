a706a4e7d828993ff082d16ebdb484cdf63e1bbb
0xAdafang <adafang@proton.me>
2025-10-24 19:54:21 -0400

    architecture + debut login

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = environment.apiUrl + '/api/auth';
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials: { username : string, password: string }):
+  Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/signin`,credentials);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+}
